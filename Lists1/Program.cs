8150226e2d6344efe9a7a6248b05cab241aa8a39
cang8 <0.gizemgunes@gmail.com>
2024-10-21 20:29:00 +0300

    added-lists1

---
+++ Lists1/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Lists1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> guests = new List<string>();
+            int i = 1;
+            while (true)
+            {
+                Console.Write("Bir davetli ismi giriniz (bitirmek icin 'exit' yaziniz):");
+                string name = Console.ReadLine();
+
+                if (name.ToLower() == "exit")
+                {
+                    break;
+                }
+
+                if (!string.IsNullOrEmpty(name))
+                {
+                    guests.Add(name);
+                }
+
+            }
+            Console.WriteLine("\n** Davetliler **");
+            foreach (string guest in guests)
+            {
+                Console.WriteLine(i + "-" + guest);
+                i++;
+            }
+        }
+    }
+}
