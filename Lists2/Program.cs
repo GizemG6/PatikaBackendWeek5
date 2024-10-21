b7dcdd5b411c117790828fbb45017389bddfe04b
cang8 <0.gizemgunes@gmail.com>
2024-10-21 20:44:47 +0300

    added-lists2

---
+++ Lists2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Xml.Linq;
+
+namespace Lists2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> coffees = new List<string>(); // create coffees list
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine($"Kahve {i}:"); // print Kahve 1:  Kahve 2:
+                string coffee = Console.ReadLine();
+
+                if (!string.IsNullOrEmpty(coffee)) // string control
+                {
+                    coffees.Add(coffee); // add items to coffees
+                }
+            }
+
+            Console.WriteLine("Girilen Kahve Isimleri:");
+
+            foreach (string c in coffees)
+            {
+                Console.WriteLine(c); // print coffees
+            }
+        }
+    }
+}
