7dfbe8ddeef8928ecf0a429aea59313e22a6a408
cang8 <0.gizemgunes@gmail.com>
2024-10-22 16:29:49 +0300

    added-final

---
+++ Final/Car.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final
+{
+    public class Car
+    {
+        // properties
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public int SerialNumber { get; set; }
+        public DateTime ProductionDate { get; set; }
+        public int DoorCount { get; set; }
+        public Car()
+        {
+            ProductionDate = DateTime.Now; // constructor method automatically assigns the production date when the car is manufactured
+        }
+    }
+}
---
+++ Final/Program.cs
@@ -0,0 +1,76 @@
+﻿namespace Final
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Car> cars = new List<Car>();
+
+            while (true)
+            {
+                Console.Write("Araba üretmek istiyor musunuz? (e/h): ");
+                string answer = Console.ReadLine().ToLower(); // using ToLower() to avoid uppercase/lowercase differences
+
+                if (answer == "h")
+                {
+                    // if the answer is no, the program ends and list of cars is printed
+                    Console.WriteLine("\nÜretilen Arabalar:");
+                    foreach (var car in cars)
+                    {
+                        Console.WriteLine($"Seri Numarası: {car.SerialNumber}, Marka: {car.Brand}");
+                    }
+                    break;
+                }
+                else if (answer == "e")
+                {
+                    // car production
+                    Car car = new Car();
+
+                    Console.Write("Seri Numarası giriniz: ");
+                    car.SerialNumber = int.Parse(Console.ReadLine());
+
+                    Console.Write("Marka giriniz: ");
+                    car.Brand = Console.ReadLine();
+
+                    Console.Write("Model giriniz: ");
+                    car.Model = Console.ReadLine();
+
+                    Console.Write("Renk giriniz: ");
+                    car.Color = Console.ReadLine();
+
+                // warning and re-entry when non-numeric value is entered for door number
+                doorCount:
+                    Console.Write("Kapı sayısını giriniz: ");
+                    try
+                    {
+                        car.DoorCount = int.Parse(Console.ReadLine());
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("Geçersiz giriş! Lütfen kapı sayısı için sayısal bir değer giriniz.");
+                        goto doorCount; // use goto to ask for the door number again in case of an error
+                    }
+
+                    // adding the created car object to the cars list
+                    cars.Add(car);
+
+                    if (answer == "h")
+                    {
+                        // if the answer is no, the program ends and list of cars is printed
+                        Console.WriteLine("\nÜretilen Arabalar:");
+                        foreach (var item in cars)
+                        {
+                            Console.WriteLine($"Seri Numarası: {item.SerialNumber}, Marka: {item.Brand}");
+                        }
+                        break;
+                    }
+                }
+                else
+                {
+                    // if an invalid answer is given, warn the user and ask them to log in again
+                    Console.WriteLine("Geçersiz cevap! Lütfen 'e' veya 'h' giriniz.");
+                }
+            }
+        }
+    }
+}
